ba44391440bcc053afd902f7bd2cc84d5d0d2650
Quansheng.Xiong <Quansheng.Xiong@fdmgroup.com>
2019-01-31 14:11:23 -0500

    connected to backend

--- src/components/home-page/SearchBox.js
+++ src/components/home-page/SearchBox.js
@@ -1,39 +1,125 @@
-import React, {Fragment, Component} from 'react';
-import { withRouter } from 'react-router-dom'
+import React, {
+  Fragment,
+  Component
+} from 'react';
+import {
+  withRouter
+} from 'react-router-dom'
+import axios from 'axios';
+import MySnackbarContentWrapper from '../CustomAlert';
+import Snackbar from '@material-ui/core/Snackbar';
 
-class SearchBox extends Component{
+class SearchBox extends Component {
   constructor(props) {
     super(props);
-    this.state = {url: ''};
-    this.handleChange = this.handleChange.bind(this);
-    this.handleSubmit = this.handleSubmit.bind(this);
+    this.state = {
+      url: '',
+      open: false,
+      errorMessage: ''
+    };
   }
 
-  handleChange(event) {
-    this.setState({url: event.target.value});
+  handleChange = (event) => {
+    this.setState({
+      url: event.target.value
+    });
   }
 
-  handleSubmit(event) {
+  goToResults = (res) => {
+    console.log(res);
+    this.props.history.push({
+      pathname: `/results/${res.data.beatmapSetId}`,
+      state: {
+        data: res.data
+      }
+    });
+  }
+
+  handleClose = () => {
+    this.setState({
+      open: false
+    });
+  };
+
+  handleError = (error) => {
+    this.setState({
+      open: true,
+      errorMessage: error.response.data
+    });
+  }
+
+  handleSubmit = (event) => {
     event.preventDefault();
-    this.props.history.push(`/results/${this.state.url}`);
+    axios.get('http://localhost:8080/mod', {
+        params: {
+          url: this.state.url
+        }
+      })
+      .then(this.goToResults)
+      .catch(this.handleError);
+
   }
 
-  render(){
+  render() {
+    const {
+      open,
+      errorMessage
+    } = this.state;
+    return ( < Fragment >
+      <
+      form onSubmit = {
+        this.handleSubmit
+      }
+      className = "form-inline" >
+      <
+      div className = "form-group mx-lg-4 mb-2" >
+      <
+      label htmlFor = "mapLink"
+      className = "sr-only" > map link < /label> <
+      input required name = "map"
+      onChange = {
+        this.handleChange
+      }
+      value = {
+        this.state.url
+      }
+      type = "text"
+      className = "form-control"
+      id = "mapLink"
+      placeholder = "map link" > < /input> <
+      /div> <
+      button type = "submit"
+      className = "btn btn-primary mb-2" > Submit < /button> <
+      /form> <
+      Snackbar anchorOrigin = {
+        {
+          vertical: 'top',
+          horizontal: 'center',
+        }
+      }
+      open = {
+        open
+      }
+      autoHideDuration = {
+        5000
+      }
+      onClose = {
+        this.handleClose
+      } >
+      <
+      MySnackbarContentWrapper onClose = {
+        this.handleClose
+      }
+      variant = "error"
+      message = {
+        errorMessage
+      }
+      /> <
+      /Snackbar>
 
-  return (<Fragment>
-    <form onSubmit={this.handleSubmit} className="form-inline">
-    <div className="form-group mx-lg-4 mb-2">
-    <label htmlFor="mapLink" className="sr-only">map link</label>
-    <input required
-    name="map"
-    onChange={this.handleChange}
-    value={this.state.url}
-    type="text" className="form-control" id="mapLink"
-    placeholder="map link"></input>
-  </div>
-  <button type="submit" className="btn btn-primary mb-2">Submit</button>
-</form>
-    </Fragment>);
+      <
+      /Fragment>
+    );
   };
 };
 
--- src/components/result-page/ResultsPage.js
+++ src/components/result-page/ResultsPage.js
@@ -1,21 +1,21 @@
 import React from 'react';
 import TabContainer from './TabContainer';
-import data from './data.json';
-import CircularProgress from '@material-ui/core/CircularProgress';
+// import data from './data.json';
+// import CircularProgress from '@material-ui/core/CircularProgress';
 import AppBar from '@material-ui/core/AppBar';
 import Typography from '@material-ui/core/Typography';
 import Button from '@material-ui/core/Button';
 
 const ResultsPage = (props) => {
-  console.log(props.match.params.mapId);
+  console.log(props);
       return (
         <div>
         <AppBar position="static" style={{paddingTop: "1em", paddingBottom: "1em"}}>
-        <Typography variant="h6" color="inherit" noWrap>
-            <Button style={{color: 'white'}} size="large">ai mod</Button>
+          <Typography variant="h5" color="inherit" noWrap>
+            <Button onClick={() => props.history.push('')}  style={{color: 'white'}}>ai mod</Button>
           </Typography>
         </AppBar>
-        <TabContainer data={data}/>
+        <TabContainer data={props.location.state.data}/>
         </div>
       );
 }
